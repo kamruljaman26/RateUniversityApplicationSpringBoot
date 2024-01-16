893efa47c7e5401f4778ccfe9d1155a3032bfa00
Kamrul Jaman <rakibul_islam@uni-koblenz.de>
2024-01-17 03:12:22 +0600

    Remove old feedback: The system should automatically remove any feedback that has been created more than 1 year ago.

--- src/main/java/feedback/application/feedback/repository/FeedbackRepository.java
+++ src/main/java/feedback/application/feedback/repository/FeedbackRepository.java
@@ -4,7 +4,9 @@ package feedback.application.feedback.repository;
 import feedback.application.feedback.model.Feedback;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.time.LocalDate;
 
 @Repository
 public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
+    void deleteByCreatedAtBefore(LocalDate date);
 }
